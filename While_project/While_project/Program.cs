ea8b25f47d872ee9516c589b2fdd904129cdb7d7
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-09 11:16:12 -0700

    While program Comments

--- While_project/While_project/Program.cs
+++ While_project/While_project/Program.cs
@@ -10,19 +10,20 @@ namespace While_project
     {
         static void Main(string[] args)
         {
-            int timer = 1;
-            while(timer <= 10)
+            int timer = 1;      //Create integer "timer"
+            while(timer <= 10)  //Create while clause for when timer is <= 10
             {
-                Console.WriteLine(timer);
-                timer++;
+                Console.WriteLine(timer);   //Show user timer count
+                timer++;                    //With each loop increase value of timer by 1;
             }
-            do
+
+            do                              //Create a do loop
             {
-                Console.WriteLine(timer);
-                timer++;
+                Console.WriteLine(timer);   //Show user timer count
+                timer++;                    //With each loop increase value of timer by 1;
             }
-            while (timer >= 10 && timer < 21);
-            Console.ReadLine();
+            while (timer >= 10 && timer < 21);  //Create a while clause that causes above do loop to run when timer is >=10 and < 21
+            Console.ReadLine();                 //End ReadLine() to keep program from shutting.
         }
     }
 }
