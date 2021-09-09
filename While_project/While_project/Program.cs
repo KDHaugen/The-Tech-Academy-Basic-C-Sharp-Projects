e730e9d0b382ef8aa70761da7db929e6d295fe49
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-08 22:16:48 -0700

    While project

---
+++ While_project/While_project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timer = 1;
+            while(timer <= 10)
+            {
+                Console.WriteLine(timer);
+                timer++;
+            }
+            do
+            {
+                Console.WriteLine(timer);
+                timer++;
+            }
+            while (timer >= 10 && timer < 21);
+            Console.ReadLine();
+        }
+    }
+}
