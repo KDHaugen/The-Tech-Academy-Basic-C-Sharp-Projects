4d2809dfa3487e8827ed5938149e7e8a3b4629be
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-10 19:22:33 -0700

    class method attempt

---
+++ Class and Method assignment/Class and Method assignment/Operator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_assignment
+{
+    class Operator
+    {
+        public Operator()
+        {
+            userInt = Program.userInp;
+            userDouble = Program.userInp * 2;
+            userTriple = Program.userInp * 3;
+            userQuad = Program.userInp * 4;
+        }
+        public static int userInt { get; set; }
+        public static int userDouble {get; set;}
+        public static int userTriple {get; set;}
+        public static int userQuad {get; set;}
+
+    }
+}
---
+++ Class and Method assignment/Class and Method assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_assignment
+{
+    class Program
+    {
+        public static int userInp = Convert.ToInt32(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Operator.userDouble);
+            Console.WriteLine(Operator.userTriple);
+            Console.WriteLine(Operator.userQuad);
+            Console.ReadLine();
+        }
+    }
+}
