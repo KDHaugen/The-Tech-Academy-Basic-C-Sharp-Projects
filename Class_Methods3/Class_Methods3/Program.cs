7b1c999f1f4a4d7f2fba7cfab634fe223ea56484
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-11 15:15:45 -0700

    twoIntergerMethod

---
+++ Class_Methods3/Class_Methods3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                //Instatiate object sumObj from class twoIntOperator.
+            twoIntOperator sumObj = new twoIntOperator();
+            
+                //Prompt and receive an interger from user.
+            Console.WriteLine("Please enter a positive whole number.");
+            int intOne = Convert.ToInt32(Console.ReadLine());
+                
+                //Prompt and receieve a second integer from user. Can be left blank.
+            Console.WriteLine("Please enter another positive whole number. You may leave this submission blank.");
+            int intTwo = Convert.ToInt32(Console.ReadLine()+0); //Add "+0" so variable always results in interger even if submission is blank.
+
+                //Call method twoIntMethod() to find sum of intergers and display them on line.
+            Console.WriteLine("Your two numbers added together equal " + sumObj.twoIntMethod(intOne, intTwo));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class_Methods3/Class_Methods3/twoIntOperator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods3
+{
+    class twoIntOperator
+    {
+        public int twoIntMethod(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
