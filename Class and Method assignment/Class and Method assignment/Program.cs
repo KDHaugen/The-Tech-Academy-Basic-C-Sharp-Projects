36ad4e9027cd76ef1956a6e2a3bd551759451be2
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-11 13:32:44 -0700

    method syntax

--- Class and Method assignment/Class and Method assignment/Program.cs
+++ Class and Method assignment/Class and Method assignment/Program.cs
@@ -8,12 +8,28 @@ namespace Class_and_Method_assignment
 {
     class Program
     {
-        public static int userInp = Convert.ToInt32(Console.ReadLine());
+        static void intDouble(int userInp)
+        {
+            int inputDouble = userInp * 2;
+            Console.WriteLine("Your number doubled is " + inputDouble);
+        }
+        static void intTriple(int userInp)
+        {
+            int inputTriple = userInp * 3;
+            Console.WriteLine("Your number tripled is " + inputTriple);
+        }
+        static void intQuadrupl(int userInp)
+        {
+            int inputQuadruple = userInp * 4;
+            Console.WriteLine("Your number quadrupled is " + inputQuadruple);
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine(Operator.userDouble);
-            Console.WriteLine(Operator.userTriple);
-            Console.WriteLine(Operator.userQuad);
+            Console.WriteLine("Please pick a whole and positive number.");
+            int userInp = Convert.ToInt32(Console.ReadLine());
+
+            intDouble(1);
             Console.ReadLine();
         }
     }
