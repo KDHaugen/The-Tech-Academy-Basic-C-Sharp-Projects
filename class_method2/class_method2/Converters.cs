85511b50175376f45218f67da2ff6c3e615a51eb
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-11 14:55:36 -0700

    Converter method added.

---
+++ class_method2/class_method2/Converters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_method2
+{
+    class Converters
+    {   
+            //Create method that doubles interger given
+        public int intDoubler(int x)
+        {
+            return x * 2;
+        }
+            //Create method to convert given decimal to interger.
+        public int decToInt(decimal x)
+        {
+            return Convert.ToInt32(x);
+        }
+        //Create method to convert given string to interger.
+        public int stringToInt(string x)
+        {
+            return Convert.ToInt32(x);
+        }
+    }
+}
---
+++ class_method2/class_method2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_method2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+                //Instatiating a new object from class Converters
+            Converters obj1 = new Converters();
+
+                //Prompt user for Integer to double. Call intDoubler() from Converters obj1
+            Console.WriteLine("Please input a whole and positive number to double");
+            int userInterger = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number doubled is " + obj1.intDoubler(userInterger));
+
+                //Prompt user for Decimal to convert to Interger. Call decToInt() from Converters obj1
+            Console.WriteLine("Please enter a decimal value to convert to an integer.");
+            decimal userDecimal = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Your decimal as an interger is " + obj1.decToInt(userDecimal));
+
+                //Prompt user for String to convert to Interger. Call stringToInt() from Converters obj1
+            Console.WriteLine("Please enter a string value to convert to an integer.");
+            string userString = Console.ReadLine();
+            Console.WriteLine("Your string as an interger is " + obj1.stringToInt(userString));
+
+            Console.ReadLine();
+        }
+    }
+}
