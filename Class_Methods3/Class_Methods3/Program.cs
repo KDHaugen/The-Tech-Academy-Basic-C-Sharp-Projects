c227cef67599aa3fc3f528559af94692b739a16f
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-11 16:01:48 -0700

    sum method

--- Class_Methods3/Class_Methods3/Program.cs
+++ Class_Methods3/Class_Methods3/Program.cs
@@ -10,21 +10,31 @@ namespace Class_Methods3
     {
         static void Main(string[] args)
         {
+            try
+            {
                 //Instatiate object sumObj from class twoIntOperator.
-            twoIntOperator sumObj = new twoIntOperator();
-            
+                twoIntOperator sumObj = new twoIntOperator();
+
                 //Prompt and receive an interger from user.
-            Console.WriteLine("Please enter a positive whole number.");
-            int intOne = Convert.ToInt32(Console.ReadLine());
-                
+                Console.WriteLine("Please enter a positive whole number.");
+                int intOne = Convert.ToInt32(Console.ReadLine());
+
                 //Prompt and receieve a second integer from user. Can be left blank.
-            Console.WriteLine("Please enter another positive whole number. You may leave this submission blank.");
-            int intTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter another positive whole number. You may leave this submission blank.");
+                int intTwo = Convert.ToInt32(Console.ReadLine());
 
                 //Call method twoIntMethod() to find sum of intergers and display them on line.
-            Console.WriteLine("Your two numbers added together equal " + sumObj.twoIntMethod(intOne, intTwo));
+                Console.WriteLine("Your two numbers added together equal " + sumObj.twoIntMethod(intOne, intTwo));
 
-            Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Your only number you chose is " + intOne);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
