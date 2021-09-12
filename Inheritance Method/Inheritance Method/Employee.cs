1bcbc54d049898795b10ae653ed7f5e9d5ce517d
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-11 17:00:25 -0700

    Inheritence

---
+++ Inheritance Method/Inheritance Method/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Method
+{
+    class Employee : Person
+    {
+        public List<int> ID { get; set; }
+        
+    }
+}
