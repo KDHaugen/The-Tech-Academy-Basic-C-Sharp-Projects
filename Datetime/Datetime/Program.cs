b3fdfcb6ae8abfad7d45c5bd04bd5c067698c185
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-12 22:54:19 -0700

    DateTime

---
+++ Datetime/Datetime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current time and date is "+DateTime.Now);
+            Console.WriteLine("Pick a number of hours to pass in time");
+            int passHours = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("In " + passHours + " hours it'll be " + DateTime.Now.AddHours(passHours));
+            Console.ReadLine();
+        }
+    }
+}
