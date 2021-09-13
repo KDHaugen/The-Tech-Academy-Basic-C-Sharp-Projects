127cd759304787f9568acef841c5ca7e0385d060
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-13 13:16:48 -0700

    update DateTime

--- Datetime/Datetime/Program.cs
+++ Datetime/Datetime/Program.cs
@@ -10,9 +10,13 @@ namespace Datetime
     {
         static void Main(string[] args)
         {
+                //Open with current time using DateTime.Now
             Console.WriteLine("The current time and date is "+DateTime.Now);
+
+                //Prompt user to pick an interger to add hours
             Console.WriteLine("Pick a number of hours to pass in time");
             int passHours = Int32.Parse(Console.ReadLine());
+                //Use .AddHours(passHours) to add hours to DateTime.Now to show time with passHours hours added.
             Console.WriteLine("In " + passHours + " hours it'll be " + DateTime.Now.AddHours(passHours));
             Console.ReadLine();
         }
