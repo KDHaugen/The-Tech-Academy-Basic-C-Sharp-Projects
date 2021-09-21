b0a1136843161ab83078c315bbec175486b5ba6e
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-09-21 14:43:37 -0700

    carinsurace

---
+++ DateOfBirth/DateOfBirth/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateOfBirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dob = Convert.ToDateTime("1989/10/22");
+            DateTime today = DateTime.Today;
+
+            var age = today - dob;
+
+            Console.WriteLine(dob);
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+    }
+}
